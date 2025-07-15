8465ef7d6ed963ad3aa4a0a96c5d63ee6e8ef124
Quanglongphamnguyen <63998643+Quanglongphamnguyen@users.noreply.github.com>
2025-07-15 19:03:41 +0700

    Update

--- start_game.cs
+++ start_game.cs
@@ -19,7 +19,7 @@ namespace ConsoleGame
         static void Main(string[] args)
         {
                                 // Đưa vào địa chỉ cụ thể nơi lưu game, ví dụ D:\Backups\.... đưa cụ thể vào folder này, để lưu và load file save.
-            const string path = @"";
+            const string path = @"D:\Developer\Learning_CSharp\ConsoleGame\savegame.txt";
 
             bool run = true;
             bool menu = true;
